9b04513f62a5a404661377ae1417b70ad2a478d1
liugh <liugh@liughdeMacBook-Pro.local>
2021-09-15 08:00:22 +0800

    project updata

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <div class="container">
-    <global-header :user="currentUser"></global-header>
-    <loader
-      v-if="isLoading"
-      text="拼命加载中"
-    ></loader>
-    <!-- <message type="error" :message="error.message" v-if="error.status"></message> -->
-    <router-view></router-view>
-    <footer class="text-center py-4 text-secondary bg-light mt-6">
-      <small>
-        <ul class="list-inline mb-0">
-          <li class="list-inline-item">© 2020 者也专栏</li>
-          <li class="list-inline-item">课程</li>
-          <li class="list-inline-item">文档</li>
-          <li class="list-inline-item">联系</li>
-          <li class="list-inline-item">更多</li>
-        </ul>
-      </small>
-    </footer>
+  <div>
+    <div class="header-bg">
+      <div class="container">
+        <global-header :user="currentUser"></global-header>
+      </div>
+    </div>
+    <div class="container">
+      <loader
+        v-if="isLoading"
+        text="拼命加载中"
+      ></loader>
+      <router-view></router-view>
+    </div>
+    <div class="footer-bg">
+      <div class="container">
+        <footer class="text-center py-4 text-secondary bg-light mt-6">
+        <small>
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item">© 2020 者也专栏</li>
+            <li class="list-inline-item">课程</li>
+            <li class="list-inline-item">文档</li>
+            <li class="list-inline-item">联系</li>
+            <li class="list-inline-item">更多</li>
+          </ul>
+        </small>
+      </footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, watch } from 'vue'
+import { computed, defineComponent, watch } from 'vue'
 import { useStore } from 'vuex'
-import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader from './components/GlobalHeader.vue'
 import Loader from './components/Loader.vue'
@@ -58,5 +66,10 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+  .header-bg{
+    background: #0d6efd;
+  }
+  .footer-bg{
+    background: #f8f9fa;
+  }
 </style>
